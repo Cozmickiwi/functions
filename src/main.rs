2bf0b7876cbd94b4fffea4f1e71a5d766f375b04
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 21:14:13 +1300

    pass second function second argument and change print statement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5);
+    another_function(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn another_function(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
