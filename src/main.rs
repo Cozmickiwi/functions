aebcbfca57c33f6b1d3680f5da90734fbcdd4eb0
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 21:15:24 +1300

    change second function name to print_labeled_measurement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5, 'h');
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function(value: i32, unit_label: char) {
+fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
